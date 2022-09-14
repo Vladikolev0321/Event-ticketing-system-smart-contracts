701728339dd547b0b95d3dded147c7503ff1b25a
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-09-14 15:25:19 +0300

    feat: Change in create event functionality

--- Event-ticketing-system/contracts/EventManager.sol
+++ Event-ticketing-system/contracts/EventManager.sol
@@ -8,17 +8,27 @@ contract EventManager is ERC1155{
     struct Event {
         uint eventId;
         // mapping(uint => uint) ticketIdsAmounts;
-        uint[] ticketIds;
-        uint[] ticketAmounts;
+        // uint[] ticketIds;
+        // uint[] ticketAmounts;
+        // Ticket[] tickets;
+        mapping(uint => Ticket) tickets;
     }
 
-    constructor() public ERC1155("https://game.example/api/item/{id}.json") {
+    struct Ticket {
+        uint eventId;
+        uint ticketId;
+        uint amount;
     }
 
     mapping(uint => Event) public events; // id => event
+    // Event[] public events;
     uint public eventsCount;
 
     event EventCreated(address _address);
+
+
+    constructor() public ERC1155("https://game.example/api/item/{id}.json") {
+    }
 
     function createEvent(address to, uint256[] memory ids, uint256[] memory amounts) public {
 
@@ -28,26 +38,42 @@ contract EventManager is ERC1155{
         _mintBatch(to, ids, amounts, "");
 
         eventsCount++;
-        events[eventsCount] = Event (
-            eventsCount,
-            // ticketIdsAmounts
-            ids,
-            amounts
-        );// ticketId and corresponding amount
+        Event storage newEvent = events[eventsCount];
+        newEvent.eventId = eventsCount;
+
+        for (uint i=0; i < ids.length; i++) {
+            newEvent.tickets[ids[i]] = Ticket(eventsCount, ids[i], amounts[i]);
+        }
+
     }
 
-    function getEventInfo(uint eventId) public view returns(uint[] memory, uint[] memory){
-        return (events[eventId].ticketIds, events[eventId].ticketAmounts);
+    function getEventInfo(uint eventId, uint ticketId) public view returns(uint){
+        // return (events[eventId].ticketIds, events[eventId].ticketAmounts);
+        return (events[eventId].tickets[ticketId].amount);
     }
-
-
 
     // buyTickets()
+    function buyTickets(uint[] memory ticketIds) external payable {
+
+
+    }
 
     //editEventInfo() // maybe edit from metadata
 
     // addTickets()
+    // function addTickets(uint eventId, address to, uint256[] memory ids, uint256[] memory amounts) public {
+        
+    //     // mint tickets
+    //     _mintBatch(to, ids, amounts, "");
+    //     for (uint i=0; i < ids.length; i++) {
+    //         events[eventId].ticketIds.push(ids[i]);
+    //     }
+    //     for (uint i=0; i < ids.length; i++) {
+    //         events[eventId].ticketAmounts.push(amounts[i]);
+    //     }
 
+
+    // }
     //setTicketForSale()
 
 
