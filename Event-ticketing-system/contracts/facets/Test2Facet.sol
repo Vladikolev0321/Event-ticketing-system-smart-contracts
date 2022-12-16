165606aca67ff1122bd1eddbf88b103df0d5e1f5
Vladikolev0321 <vladi.kolev2003@gmail.com>
2022-12-16 10:44:40 +0200

    feat: Add diamond setup

---
+++ Event-ticketing-system/contracts/facets/Test2Facet.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "../libraries/LibEventStorage.sol";
+
+contract Test2Facet is ERC1155Holder{
+
+    event Listed(
+		uint listingId,
+		address seller,
+		address token,
+		uint tokenId,
+		uint price
+	);
+
+    event Sale(
+		uint listingId,
+		address buyer,
+		address token,
+		uint tokenId,
+		uint price
+	);
+
+    AppStorage internal s;
+
+    //listTicket()
+    function listTicket(address token, uint tokenId, uint amount, uint price) external {
+        // s = LibEventStorage.diamondStorage();
+        IERC1155(token).safeTransferFrom(msg.sender, address(this), tokenId, amount, "");
+
+		Listing memory listing = Listing(
+			ListingStatus.Active,
+			msg.sender,
+			token,
+			tokenId,
+			price
+		);
+
+		s._listingId++;
+		s.listingCount++;
+
+		s.listings[s._listingId] = listing;
+
+		emit Listed(
+			s._listingId,
+			msg.sender,
+			token,
+			tokenId,
+			price
+		);
+	}
+
+    //buyTicket()
+    function buyTicket(uint listingId) external payable {
+        // s = LibEventStorage.diamondStorage();
+		Listing storage listing = s.listings[listingId];
+
+		require(msg.sender != listing.seller, "Seller cannot be buyer");
+		require(listing.status == ListingStatus.Active, "Listing is not active");
+
+		require(msg.value >= listing.price, "Insufficient payment");
+
+		listing.status = ListingStatus.Sold;
+
+        IERC1155(listing.token).safeTransferFrom(address(this), msg.sender, listing.tokenId, 1, ""); // ?????
+
+		payable(listing.seller).transfer(listing.price);
+
+		emit Sale(
+			listingId,
+			msg.sender,
+			listing.token,
+			listing.tokenId,
+			listing.price
+		);
+	}
+
+    function getListing(uint listingId) public view returns (Listing memory) {
+        // s = LibEventStorage.diamondStorage();
+		return s.listings[listingId];
+	}
+}
